b4e5f46b29744dc8d03de5a79978e13944a8ed7c
DaanGamesDG <dwklarenbeek@gmail.com>
2021-08-16 22:51:06 +0200

    required perms update

--- config.ts
+++ config.ts
@@ -4,6 +4,6 @@ export const discordServer = "https://discord.gg/46v9tr3Wxp";
 export const apis = ["https://api.stereo-bot.tk", "https://api2.stereo-bot.tk"];
 
 export const invites = [
-	"https://discord.com/oauth2/authorize?client_id=745665203777306664&scope=bot%20applications.commands&permissions=11889984",
-	"https://discord.com/oauth2/authorize?client_id=769595543151443970&scope=bot%20applications.commands&permissions=11889984",
+	"https://discord.com/oauth2/authorize?client_id=745665203777306664&scope=bot%20applications.commands&permissions=3411200",
+	"https://discord.com/oauth2/authorize?client_id=769595543151443970&scope=bot%20applications.commands&permissions=3411200",
 ];
