124b78e68195e13dd25049fe313b60d22180a6e0
DaanGamesDG <daan@daangamesdg.xyz>
2021-12-03 19:53:17 +0100

    fix: fix landing page after refactor of languageHandler

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,7 @@
-import type { GetServerSidePropsContext, GetServerSidePropsResult, NextPage } from "next";
-import { getTranslations, LandingTranslations } from "../lib";
+import type { NextPage } from "next";
 
-const Landing: NextPage<{ t: LandingTranslations }> = ({ t }) => {
-	return <div>{t.test}</div>;
+const Landing: NextPage = () => {
+	return <div>{"a"}</div>;
 };
-
-export function getServerSideProps(ctx: GetServerSidePropsContext): GetServerSidePropsResult<{ t: LandingTranslations }> {
-	return {
-		props: {
-			t: getTranslations<LandingTranslations>(ctx.locale, "landing")
-		}
-	};
-}
 
 export default Landing;
