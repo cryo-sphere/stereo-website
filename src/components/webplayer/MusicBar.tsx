9d6d834fb0be6bfa8bd427f650a1fe040f10ff47
DaanGamesDG <daan@daangamesdg.xyz>
2021-12-03 20:36:28 +0100

    feat(Musicbar): add song info

--- src/components/webplayer/MusicBar.tsx
+++ src/components/webplayer/MusicBar.tsx
@@ -1,5 +1,19 @@
 import React from "react";
+import Image from "next/image";
+import { Tippy } from "../Tippy";
 
 export const MusicBar: React.FC = () => {
-	return <div className="musicbar"></div>;
+	return (
+		<div className="musicbar">
+			<Tippy content="Open in browser">
+				<a href="https://youtube.com/watch?v=C4mBfkNVnEk" target="_blank" rel="noreferrer" className="musicbar-song-info">
+					<Image alt="song image" src="https://img.youtube.com/vi/C4mBfkNVnEk/default.jpg" width={66.7} height={50} />
+					<div className="musicbar-song-text">
+						<h3>Placeholder song title</h3>
+						<p>Placeholder song source</p>
+					</div>
+				</a>
+			</Tippy>
+		</div>
+	);
 };
