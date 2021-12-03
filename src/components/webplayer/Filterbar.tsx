60a4df4227cbd372dbf5b5956a475eef730ac667
DaanGamesDG <daan@daangamesdg.xyz>
2021-12-03 20:01:07 +0100

    feat(Filterbar): add correct filters to list

--- src/components/webplayer/Filterbar.tsx
+++ src/components/webplayer/Filterbar.tsx
@@ -7,107 +7,43 @@ export const Filterbar: React.FC = () => {
 			<hr className="filterbar-separator"></hr>
 			<ul className="filterbar-list">
 				<li>
+					<button className="filterbar-item">Karaoke</button>
+				</li>
+				<li>
+					<button className="filterbar-item">Tremelo</button>
+				</li>
+				<li>
+					<button className="filterbar-item">Pop</button>
+				</li>
+				<li>
+					<button className="filterbar-item">8D</button>
+				</li>
+				<li>
+					<button className="filterbar-item">Slowed</button>
+				</li>
+				<li>
 					<button className="filterbar-item">Vaporwave</button>
 				</li>
 				<li>
-					<button className="filterbar-item filterbar-active">Slowed</button>
+					<button className="filterbar-item">Nightcore</button>
 				</li>
 				<li>
-					<button className="filterbar-item filterbar-active">Slowed</button>
+					<button className="filterbar-item">Soft</button>
 				</li>
 				<li>
-					<button className="filterbar-item filterbar-active">Slowed</button>
+					<button className="filterbar-item">Bassboost: Extreme</button>
 				</li>
 				<li>
-					<button className="filterbar-item filterbar-active">Slowed</button>
+					<button className="filterbar-item">Bassboost: Hard</button>
 				</li>
 				<li>
-					<button className="filterbar-item filterbar-active">Slowed</button>
+					<button className="filterbar-item">Bassboost: Medium</button>
 				</li>
 				<li>
-					<button className="filterbar-item filterbar-active">Slowed</button>
+					<button className="filterbar-item">Bassboost: Low</button>
 				</li>
 				<li>
-					<button className="filterbar-item filterbar-active">Slowed</button>
-				</li>
-				<li>
-					<button className="filterbar-item filterbar-active">Slowed</button>
-				</li>
-				<li>
-					<button className="filterbar-item filterbar-active">Slowed</button>
-				</li>
-				<li>
-					<button className="filterbar-item filterbar-active">Slowed</button>
-				</li>
-				<li>
-					<button className="filterbar-item filterbar-active">Slowed</button>
-				</li>
-				<li>
-					<button className="filterbar-item filterbar-active">Slowed</button>
-				</li>
-				<li>
-					<button className="filterbar-item filterbar-active">Slowed</button>
-				</li>
-				<li>
-					<button className="filterbar-item filterbar-active">Slowed</button>
-				</li>
-				<li>
-					<button className="filterbar-item filterbar-active">Slowed</button>
-				</li>
-				<li>
-					<button className="filterbar-item filterbar-active">Slowed</button>
-				</li>
-				<li>
-					<button className="filterbar-item filterbar-active">Slowed</button>
-				</li>
-				<li>
-					<button className="filterbar-item filterbar-active">Slowed</button>
-				</li>
-				<li>
-					<button className="filterbar-item filterbar-active">Slowed</button>
-				</li>
-
-				<li>
-					<button className="filterbar-item filterbar-active">Slowed</button>
-				</li>
-				<li>
-					<button className="filterbar-item filterbar-active">Slowed</button>
-				</li>
-				<li>
-					<button className="filterbar-item filterbar-active">Slowed</button>
-				</li>
-				<li>
-					<button className="filterbar-item filterbar-active">Slowed</button>
-				</li>
-				<li>
-					<button className="filterbar-item filterbar-active">Slowed</button>
-				</li>
-				<li>
-					<button className="filterbar-item filterbar-active">Slowed</button>
-				</li>
-				<li>
-					<button className="filterbar-item filterbar-active">Slowed</button>
-				</li>
-				<li>
-					<button className="filterbar-item filterbar-active">Slowed</button>
-				</li>
-				<li>
-					<button className="filterbar-item filterbar-active">Slowed</button>
-				</li>
-				<li>
-					<button className="filterbar-item filterbar-active">Slowed</button>
-				</li>
-				<li>
-					<button className="filterbar-item filterbar-active">Slowed</button>
-				</li>
-				<li>
-					<button className="filterbar-item filterbar-active">Slowed</button>
-				</li>
-				<li>
-					<button className="filterbar-item filterbar-active">Slowed</button>
-				</li>
-				<li>
-					<button className="filterbar-item filterbar-active">Slowed</button>
+					<button className="filterbar-item">none</button>
 				</li>
 			</ul>
 		</div>
