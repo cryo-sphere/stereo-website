869ec02c0ca1bb880aa9a69c97af7db3619ceede
DaanGamesDG <daan@daangamesdg.xyz>
2022-04-27 22:43:30 +0200

    chore: domain update

--- config.ts
+++ config.ts
@@ -1,9 +1,9 @@
-export const statusPage = "https://status.stereo-bot.tk";
+export const statusPage = "https://status.stereo-bot.xyz";
 export const discordServer = "https://discord.gg/46v9tr3Wxp";
 export const github = "https://github.com/stereo-bot";
-export const crowdin = "https://translate.stereo-bot.tk";
+export const crowdin = "https://translate.stereo-bot.xyz";
 
-export const apis = ["https://api.stereo-bot.tk", "https://api2.stereo-bot.tk"];
+export const apis = ["https://api.stereo-bot.xyz", "https://api2.stereo-bot.xyz"];
 
 export const invites = [
 	"https://discord.com/oauth2/authorize?client_id=745665203777306664&scope=bot%20applications.commands&permissions=3411200",
